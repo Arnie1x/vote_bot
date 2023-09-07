d45257b0fa02e73ec0f8d316aea7869c2ed771b4
Arnie <arnie752@gmail.com>
2023-09-07 12:35:40 +0300

    the bot can fully complete a voting process

--- main.py
+++ main.py
@@ -1,49 +1,89 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+for i in range(0, 1000):
+    name = 'Lucas Kibet'
 
-name = 'Lucas Kibet'
+    # Get email first
+    mailDriver = webdriver.Firefox()
+    mailAction = ActionChains(mailDriver)
+    mailDriver.get("https://www.minuteinbox.com/")
+    mailDriver.implicitly_wait(5)
 
-mailDriver = webdriver.Chrome()
-mailDriver.get("https://10minutemail.net/")
-mailDriver.implicitly_wait(5)
+    email = mailDriver.find_element(by=By.ID, value="email").text
+    print(email)
 
-email = mailDriver.find_element(by=By.CLASS_NAME, value="mailtext").get_attribute('value')
-# text_box.send_keys("Selenium")
-# submit_button.click()
-print(email)
+    # Initiate Voting on Website
+    voteDriver = webdriver.Firefox()
+    voteAction = ActionChains(voteDriver)
+    voteDriver.get("https://cup.craydel.com/submissions/1738850155")
+    voteDriver.implicitly_wait(10)
+    elements = voteDriver.find_elements(By.CLASS_NAME, 'label-with-icon')
+    voteButton = voteDriver.find_element(By.XPATH,
+                                         '//div[@id=\'app\']/div/main/div/div/section/div/div[2]/div[2]/div[2]/a/span')
+    voteAction.move_to_element(voteButton).click().perform()
+    voteDriver.implicitly_wait(2)
 
-voteDriver = webdriver.Chrome()
-action = ActionChains(voteDriver)
-voteDriver.get("https://cup.craydel.com/submissions/1738850155")
-voteDriver.implicitly_wait(10)
-elements = voteDriver.find_elements(By.CLASS_NAME, 'label-with-icon')
-voteButton = voteDriver.find_element(By.XPATH,'//div[@id=\'app\']/div/main/div/div/section/div/div[2]/div[2]/div[2]/a/span')
-action.move_to_element(voteButton).click().perform()
-voteDriver.implicitly_wait(2)
-#
-# nameField = voteDriver.find_element(by=By.ID, value="vote_name")
-# emailField = voteDriver.find_element(by=By.ID, value="vote_email")
-#
-# nameField.send_keys(name)
-# emailField.send_keys(email)
-#
-# voteNowButton = voteDriver.find_element(By.CSS_SELECTOR, ".btn .btn-primary .v-btn .v-btn--is-elevated .v-btn--has-bg .theme--light .v-size--default")
-# voteNowButton.click()
-# voteDriver.implicitly_wait(2)
-#
-# otpField = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--0")
-# otpField1 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--1")
-# otpField2 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--2")
-# otpField3 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--3")
-# otpField4 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--4")
-# otpField5 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--5")
-#
-# otpField.send_keys('1')
-# otpField1.send_keys('2')
-# otpField2.send_keys('3')
-# otpField3.send_keys('4')
-# otpField4.send_keys('5')
-# otpField5.send_keys('6')
-# voteDriver.implicitly_wait(5)
+    nameField = voteDriver.find_element(by=By.ID, value="vote_name")
+    emailField = voteDriver.find_element(by=By.ID, value="vote_email")
+
+    nameField.send_keys(name)
+    emailField.send_keys(email)
+
+    voteDriver.implicitly_wait(1)
+    voteNowButton = voteDriver.find_element(By.XPATH, "//div[@id='app']/div[3]/div/div/div/form/div[5]/button/span")
+    voteAction.move_to_element(voteNowButton).click().perform()
+    voteDriver.implicitly_wait(2)
+
+    otpField = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--0")
+    otpField1 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--1")
+    otpField2 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--2")
+    otpField3 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--3")
+    otpField4 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--4")
+    otpField5 = voteDriver.find_element(by=By.CLASS_NAME, value="otp-field-box--5")
+
+    # Now, get the OTP code from 10minutemail
+
+    mailReceived = False
+    while not mailReceived:
+        time.sleep(60)
+        mailList = mailDriver.find_elements(by=By.TAG_NAME, value="td")
+        for mail in mailList:
+            if mail.text.__contains__('Craydel'):
+                mailReceived = True
+                mailAction.move_to_element(mail).double_click().perform()
+                mailDriver.implicitly_wait(2)
+                break
+            else:
+                print('Waiting for Mail...')
+            time.sleep(10)
+
+    mailDriver.implicitly_wait(3)
+    # mailText = mailDriver.find_element(By.XPATH, "//table[@class='email_container']/tbody/tr[6]/td/table/tbody/tr/td[2]/p[3]")
+    mailDriver.switch_to.frame(mailDriver.find_element(By.ID, 'iframeMail'))
+    mailText = mailDriver.find_elements(By.TAG_NAME, "p")
+    # print(mailText.text)
+
+    otpCode = ''
+    for text in mailText:
+        if text.text[0].startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')):
+            otpCode = text.text
+
+    otpField.send_keys(otpCode[0])
+    otpField1.send_keys(otpCode[1])
+    otpField2.send_keys(otpCode[2])
+    otpField3.send_keys(otpCode[3])
+    otpField4.send_keys(otpCode[4])
+    otpField5.send_keys(otpCode[5])
+    voteDriver.implicitly_wait(5)
+
+    placeVoteNowButton = voteDriver.find_element(By.XPATH,
+                                                 "//div[@id='app']/div[3]/div/div/div/form/div[3]/button/span")
+    voteAction.move_to_element(placeVoteNowButton).click().perform()
+    voteDriver.implicitly_wait(2)
+
+    voteDriver.close()
+    mailDriver.close()
